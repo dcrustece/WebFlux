393ab3f49739a32ed910d7b1014f9523ae9c6cf5
dcrustece <ankityono@gmail.com>
2024-09-09 20:51:38 +0530

    change the url request

--- WebFlux/WebFlux/src/main/java/com/webflux/demo/WebFlux/controller/EmployeeController.java
+++ WebFlux/WebFlux/src/main/java/com/webflux/demo/WebFlux/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/employee2")
 public class EmployeeController {
 
     private final EmployeeRepository employeeRepository;
